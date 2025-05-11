99b44897f054b2e6e9fea397503f9d51504114c7
Tecrato <pinchos792003@gmail.com>
2025-05-11 14:45:22 -0400

    Tamanio automatico para el "aviso1"

--- mini_GUI/more_objs.py
+++ mini_GUI/more_objs.py
@@ -6,17 +6,22 @@ from ..texts import Button, Text
 from .base import Base
 
 class aviso1(Base):
-    def __init__(self, pos, dire, text: str, font=None):
-        super().__init__(pos, dire, (200,50), 20, inside_limits=True)
-        self.text = text
+    def __init__(self, pos, dire, text: str, font=None, size=(200,50)) -> None:
+        txt = Text(text, 16, font, (20,size[1]//2-10), 'topleft', 'black', padding=1)
+        super().__init__(pos, dire, (max(txt.width + 40,size[0]), size[1]), 20, inside_limits=True)
+        self.text: str = text
 
         self.botones.clear()
 
         pag.draw.rect(self.surf, 'lightgrey', self.rect, 0, 20)
         pag.draw.rect(self.surf, 'black', self.rect, 1, 20)
 
-        Text(self.text, 14, font, (20,25), 'left', 'black').draw(self.surf)
+        txt.draw(self.surf)
 
     def click(self,pos):
         if self.rect.collidepoint(Vector2(pos)):
             return 'exit'
+    
+    @property
+    def width(self):
+        return self.size[0]
